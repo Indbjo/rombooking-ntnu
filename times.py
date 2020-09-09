972c7062d9e946cecc5e91ae15f357b09e660424
Lars Waage <46653859+larwaa@users.noreply.github.com>
2020-09-09 16:29:36 +0200

    :recycle: refactor and initial io support

--- times.py
+++ times.py
@@ -4,6 +4,75 @@ from dateutil.parser import parse
 from datetime import timedelta
 from datetime import datetime
 
+
+def load_schedule(filename='schedule.csv'):
+    try:
+        return pd.read_csv(filename)
+    except FileNotFoundError as e:
+        print(f'Could not find the file, initializing a new schedule\n'
+              f'Error: {e}')
+        return initialize_schedule(filename)
+
+
+class Schedule:
+    adjusted = False
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.schedule = set_schedule(filename)
+
+    def set_schedule(self, filename):
+        schedule = load_schedule(filename)
+
+        if schedule['active_day'].any() and not self.adjusted:
+            self.adjusted = True
+            return adjust_schedule(schedule)
+        return schedule
+
+    def save(self):
+        self.schedule.to_csv(self.filename)
+
+    def book(self, weekday, booking_index):
+        expected_booking_datetime = parse(self.schedule.loc[weekday, 'next_booking_datetime'][booking_index])
+        if datetime.now() > expected_booking_datetime:
+            self.schedule.loc[weekday, 'next_booking_datetime'][booking_index] = self.next_booking_datetime(weekday, booking_index)
+            return True
+        return False
+
+    def next_booking_datetime(self, weekday, booking_index):
+        now = datetime.now()
+
+        return ''
+
+
+def initialize_schedule(filename):
+    data = {
+        'weekday': [i for i in range(7)],
+        'start_time': [],
+        'duration': [],
+        'end_time': [],
+        'next_booking_datetime': [],
+        'active_day': False,
+    }
+    schedule = pd.DataFrame(data).set_index('weekday')
+    schedule.to_csv('schedule.csv')
+    print(f'Initialized a new schedule with the filename {filename}, please enter the desired booking times.')
+    return schedule
+
+
+def adjust_schedule(schedule):
+    compute_end_and_booking_times(schedule)
+    return schedule
+
+
+def set_schedule(filename='schedule.csv'):
+    schedule = load_schedule(filename)
+
+    if schedule['active_day'].any() and not schedule['adjusted'].all():
+        return adjust_schedule(schedule)
+
+
+
 df = pd.DataFrame()
 df['weekday'] = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4]
 df['start_time'] = ['08:15', '13:00', '08:15', '13:00', '08:15','13:00', '08:15', '13:00', '10:00', '13:00']
@@ -15,8 +84,13 @@ SEAT_PRIORITY = {'360E3-107': [7, 3]} # ordered list of seat number priorities, 
 
 
 def compute_end_and_booking_times(df):
-    df['end_time'] = df.apply(lambda row: (parse(f'{row.values[1]}') + timedelta(hours=parse(f'{row.values[2]}').hour, minutes=parse(f'{row.values[2]}').minute)).strftime('%H:%M'), axis=1)
-    #df['booking_weekday'], df['booking_time'] = determine_booking_weekday(df)
+    df['end_time'] = df.apply(
+        lambda row: (parse(f'{row.values[1]}') + timedelta(
+            hours=parse(f'{row.values[2]}').hour,
+            minutes=parse(f'{row.values[2]}').minute
+        )).strftime('%H:%M'),
+        axis=1
+    )
 
 
 def determine_booking_weekday(df):
@@ -56,15 +130,7 @@ def book(df, index):
     df.loc[index, 'next_booking_datetime'] = (parse(df.loc[index, 'next_booking_datetime']) + timedelta(days=7)).strftime('%Y-%m-%d %H:%M')
 
 
-
-
-print(df)
 compute_end_and_booking_times(df)
-print(df)
-print(len(df))
 
 df['next_booking_datetime'] = initialize_booking_dates(df)
 book(df, 0)
-print(df)
-
-
